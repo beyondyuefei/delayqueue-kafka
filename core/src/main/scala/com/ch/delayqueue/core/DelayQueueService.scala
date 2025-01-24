ada2ddc9ea154c9dda4922f957de5c1d49de7424
李强 <33909391@qq.com>
2025-01-24 19:12:01 +0800

    feat: add DelayService init

--- core/src/main/scala/com/ch/delayqueue/core/DelayQueueService.scala
+++
@@ -1,8 +0,0 @@
-package com.ch.delayqueue.core
-
-object DelayQueueService {
-   def executeWithFixedDelay[T] (message: Message[T], delaySeconds: Long):Boolean = {
-     // todo: send to delayqueue kafka topic
-      true
-   }
-}
---
+++ core/src/main/scala/com/ch/delayqueue/core/StreamMessage.scala
@@ -0,0 +1,5 @@
+package com.ch.delayqueue.core
+
+class StreamMessage() {
+
+}
