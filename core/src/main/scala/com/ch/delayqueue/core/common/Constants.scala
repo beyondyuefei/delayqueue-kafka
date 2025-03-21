9879a2ba277fefecccfa537f4340dc62ebcd8fe2
李强 <33909391@qq.com>
2025-03-21 18:17:37 +0800

    refactor(core): 重命名延迟队列相关主题和存储名称- 将 delayQueueInputTopic从 "Delay_Queue_Input_Topic_3" 修改为 "Delay_Queue_Input_Topic" - 将 storeName 从 "delayed-messages-store_3" 修改为 "delayed-messages-store" - 将 delayQueueOutputTopic 从 "Delay_Queue_Output_Topic_3" 修改为 "Delay_Queue_Output_Topic"

--- core/src/main/scala/com/ch/delayqueue/core/common/Constants.scala
+++ core/src/main/scala/com/ch/delayqueue/core/common/Constants.scala
@@ -1,7 +1,7 @@
 package com.ch.delayqueue.core.common
 
 object Constants {
-   val delayQueueInputTopic = "Delay_Queue_Input_Topic_3"
-   val storeName = "delayed-messages-store_3"
-   val delayQueueOutputTopic = "Delay_Queue_Output_Topic_3"
+   val delayQueueInputTopic = "Delay_Queue_Input_Topic"
+   val storeName = "delayed-messages-store"
+   val delayQueueOutputTopic = "Delay_Queue_Output_Topic"
 }
