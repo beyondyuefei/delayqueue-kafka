9c8ce113cb9a1145f3819e4f9a35430292c16e5c
李强 <33909391@qq.com>
2025-06-27 11:35:02 +0800

    refactor(starter): 移除 DelayQueueCallback 接口的 @FunctionalInterface 注解
    移除了 DelayQueueCallback 接口上的 @FunctionalInterface 注解，因为该接口只有一个方法，Java 编译器默认将其视为函数式接口。显式添加 @FunctionalInterface 注解是多余的，因此移除以简化代码结构。

--- starter/src/main/java/com/ch/delayqueue/starter/DelayQueueCallback.java
+++ starter/src/main/java/com/ch/delayqueue/starter/DelayQueueCallback.java
@@ -2,7 +2,6 @@ package com.ch.delayqueue.starter;
 
 import com.ch.delayqueue.core.Message;
 
-@FunctionalInterface
 public interface DelayQueueCallback {
     void OnDelayMessageTriggered(final Message message);
 }
